4a9cefff3fb001e7a3c1f2d3046db2b3ecdb2c8f
Andreas Henningson <henningsonandreas@gmail.com>
2020-03-08 22:37:15 +0100

    added waveform selection and changed variable names in main.js and tones.json

--- main.js
+++ main.js
@@ -12,23 +12,23 @@ $(document).ready(() => {
   });
 
   const getTone = (data, key) => {
-    let [tone] = data.filter(chosenTone => {
-      return chosenTone.note == key;
+    let [chosenTone] = data.filter(tone => {
+      return tone.toneName == key;
     });
-    playNote(tone);
+    playNote(chosenTone);
   };
 
-  function playNote(tone) {
-    console.log(tone);
+  function playNote(chosenTone) {
+    console.log(chosenTone);
     let $osc1Wave = $("input[name=osc1_waves]:checked").val();
     osc1 = ctx.createOscillator();
     osc1.type = $osc1Wave;
-    osc1.frequency.value = tone["freq"];
+    osc1.frequency.value = chosenTone["freq"];
 
     let $osc2Wave = $("input[name=osc2_waves]:checked").val();
     osc2 = ctx.createOscillator();
     osc2.type = $osc2Wave;
-    osc2.frequency.value = tone["freq"];
+    osc2.frequency.value = chosenTone["freq"];
     osc2.detune.value = 10;
 
     osc1.connect(ctx.destination);
